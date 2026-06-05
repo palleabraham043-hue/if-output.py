d6419bb92be0a07fbd8531f8e68aaca0e210fb09
palleabraham043-hue <palleabraham043@gmail.com>
2026-06-05 18:58:29 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,119 @@
+#if-codition by using comparasion opertors
+#<,>,<=,=>,!=,==
+'''a=10
+if a<1:
+    print("true")'''
+
+'''a=10
+b=30
+if a<=b:
+    print("true")'''
+
+
+'''a=20
+b=30
+if a!=b:
+    print("true")'''
+
+
+'''a=20
+b=40
+if a==b:
+    print("true")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))'''
+
+#if-condition by using logical opertors
+#and,or,not
+'''a=2
+b=4
+if a<b and b>a:
+    print("true")'''
+
+'''a=2
+b=4
+if a<b and b<a:
+    print("true")'''
+
+'''a=5
+b=7
+if a<=b and b>=a:
+    print("true")'''
+
+
+'''a=9
+b=11
+if a!=b and b==a:
+    print("true")'''
+
+'''a=5
+b=10
+if not a<b:
+    print("true")'''
+
+'''a=5
+b=10
+if not a<b or b>a:
+    print("true")'''
+
+'''a=int(input(" a value"))
+b=int(input(" b value"))
+if a<b and b>a:
+    print("true")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b or b>a:
+    print("true")'''
+
+#if-codition by using identify operators
+#is,is not
+'''a=2
+if type(a) is int:
+    print(" it is int")'''
+
+'''a=3
+if type(a) is not int:
+    print("it is int")'''
+'''a=int(input("enter the value"))
+if type(a) is int:
+    print("true")'''
+
+
+#if-codition by using membership operators
+#in,not in
+'''a=[10,20,30,40,50]
+if 50 in a:
+    print("true")'''
+
+'''a=[10,20,30,40,50]
+if 40 not in a:
+    print("true")'''
+
+'''a=int(input("a value"))
+if 40 in a:
+    print("true")'''#error
+
+
+'''a=[4,5,6,7,8,9,10]
+b=int(input("value"))
+if b in a:
+    print("true")'''
+
+
+#if-else condition by using comparision
+'''a=2
+b=4
+if a<b:
+    print("true")
+else:
+        print("false")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a>b:
+    print("less")
+else:
+    print("less")'''
+    
---
+++ output.py
@@ -0,0 +1,107 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+name=input("Eter name")
+Eter name
+====== RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/profile.py =====
+Enter nameabraham
+Enter idno220640
+enter collegenamejkc
+Enter mailidpalleabraham043@gmail.com
+Enter mobileno8008346228
+Enter percentage60%
+Traceback (most recent call last):
+  File "C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/profile.py", line 6, in <module>
+    percentage=float(input("Enter percentage"))
+ValueError: could not convert string to float: '60%'
+
+
+====== RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/profile.py =====
+Enter nameabraham
+Enter idno220640
+enter collegenamejkc
+Enter mailidpalleabraham043@gmail.com
+Enter mobileno8008346228
+Enter percentage60.0
+name:abraham
+Id no:220640
+college name:jkc
+mail id:palleabraham043@gmail.com
+mobile no:8008346228
+Traceback (most recent call last):
+  File "C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/profile.py", line 12, in <module>
+    print("percentage: {:.if}%".format(percentage))
+ValueError: Format specifier missing precision
+
+======== RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ========
+a
+10
+
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+true
+
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+true
+
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+true
+
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+a value
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+true
+
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+true
+
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+true
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+ a value10
+ b value5
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+a value3
+b value5
+true
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+ it is int
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+enter the value2
+true
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+true
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+a value2
+Traceback (most recent call last):
+  File "C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py", line 95, in <module>
+    if 40 in a:
+TypeError: argument of type 'int' is not a container or iterable
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+value4
+true
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+true
+>>> 
+============================================= RESTART: C:/Users/PALLE.MOSHE/OneDrive/Documents/Desktop/if.py ============================================
+a value3
+b value2
+less
